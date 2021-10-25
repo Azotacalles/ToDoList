2b2c8fc562386ff813e3c6c892a0df2b02817ac9
Pavel <paha-r@yandex.ru>
2021-10-25 19:29:08 +0300

    Add project files.

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<ToDoModel> toDoList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            toDoList = new BindingList<ToDoModel>()
+            {
+                new ToDoModel() { TaskName = "Task 1" },
+                new ToDoModel() { TaskName = "Task 2" }
+            };
+
+            ToDoGrid.ItemsSource = toDoList;
+            toDoList.ListChanged += ToDoList_ListChanged;
+        }
+
+        private void ToDoList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            switch (e.ListChangedType)
+            {
+                case ListChangedType.Reset:
+                    break;
+                case ListChangedType.ItemAdded:
+                    break;
+                case ListChangedType.ItemDeleted:
+                    break;
+                case ListChangedType.ItemMoved:
+                    break;
+                case ListChangedType.ItemChanged:
+                    break;
+                case ListChangedType.PropertyDescriptorAdded:
+                    break;
+                case ListChangedType.PropertyDescriptorDeleted:
+                    break;
+                case ListChangedType.PropertyDescriptorChanged:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    class ToDoModel : INotifyPropertyChanged
+    {
+        public DateTime DateTask { get; set; } = DateTime.Now;
+
+        private bool done;
+        private string taskName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string nameProp)
+        {
+            //if(PropertyChanged!= null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(nameProp));
+            //}
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameProp));
+        }
+
+        public bool Done
+        {
+            get => done;
+            set
+            {
+                if (done == value) return;
+                done = value;
+                OnPropertyChanged("Done");
+            }
+        }
+
+        public string TaskName
+        {
+            get => taskName;
+            set
+            {
+                if (taskName == value) return;
+                taskName = value;
+                OnPropertyChanged("TaskName");
+            }
+        }
+    }
+}
